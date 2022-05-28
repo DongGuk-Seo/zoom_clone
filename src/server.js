b065ffbf54f78edd04c0cf2677c4001b8c604db0
DongGuk-Seo <failnxid@gmail.com>
2022-05-28 15:27:07 +0900

    env setup

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
-import express, { application } from "express";
+import express from "express";
 
 const app = express();
 
-console.log('hello');
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public")); // application call(js) - Front End
+app.get("/", (req, res) => res.render("home")); // rendering templates
+app.get("/*", (req, res) => res.redirect("/")); // redirect to home for blocking searching folders
 
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
 app.listen(3000);
