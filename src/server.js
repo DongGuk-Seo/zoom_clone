acb85a2ad57a907417c5637269fd0b8e67ebffc5
DongGuk-Seo <failnxid@gmail.com>
2022-06-10 01:20:56 +0900

    add input

--- src/server.js
+++ src/server.js
@@ -15,13 +15,15 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app); // http server
 const wss = new WebSocket.Server({ server }); // Web Socket server , {server} is not necessary(For running http and wss simultaneously)
 
+const sockets = [];
+
 wss.on("connection", (socket) => {
+    sockets.push(socket);
     console.log("Connected to Browser")
     socket.on("close", () => console.log("Disconnected from the Browser"));
     socket.on("message", (message) => {
-        console.log(message.toString("utf8"));
+        sockets.forEach(aSocket => aSocket.send(message.toString("utf-8")));
     });
-    socket.send("hello");
 });
 
 server.listen(3000, handleListen);
