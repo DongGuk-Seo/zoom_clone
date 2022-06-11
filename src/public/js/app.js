37206d0f7118cdfbaba2f921915d23d1056c5abe
DongGuk-Seo <failnxid@gmail.com>
2022-06-11 23:55:29 +0900

    add chat

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,29 +1,39 @@
 const messageList = document.querySelector('ul');
-const messageForm = document.querySelector('form');
+const nickForm = document.querySelector('#nickname');
+const messageForm = document.querySelector('#message');
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMassage(type, payload) {
+    const msg = { type, payload }
+    return JSON.stringify(msg);
+}
 
 socket.addEventListener("open", () => {
     console.log("Connected to Server");
 });
 
 socket.addEventListener("message", (message) => {
-    console.log("New message :", message.data);
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
 });
 
 socket.addEventListener("close", () => {
     console.log("Disconnected to Server");
 });
 
-setTimeout(() => {
-    socket.send("hello from the browser!");
-}, 5000);
-
-
 function handleSubmit(event) {
     event.preventDefault();
     const input = messageForm.querySelector("input");
-    socket.send(input.value);
+    socket.send(makeMassage("new_message", input.value));
     input.value = '';
 }
 
+function handleNickSubmit(event) {
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    socket.send(makeMassage("nickname", input.value));
+}
+
 messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
