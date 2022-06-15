e3feff0a93c33a9980d80d886e8c3ff5521a99df
DongGuk-Seo <failnxid@gmail.com>
2022-06-15 23:54:13 +0900

    remove index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-// https://velog.io/@ongsim123/node.js-express-server 참조 벨로그
-const express = require('express') //express를 설치했기 때문에 가져올 수 있다.
-const app = express()
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
-app.listen(4000)
